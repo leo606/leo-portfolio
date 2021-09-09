9bf6df27f62c16fad4817bd08f06e0a4ba814d30
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-08 22:36:52 -0300

    cria Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style/Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Projetos</li>
+          <li>Recomendações</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

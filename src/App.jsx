7c9d2bc8e140e980b9019a889e2a4da650e89979
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-30 16:48:35 -0300

    useOnClickOutside

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useRef } from 'react';
 import { ThemeProvider } from 'styled-components';
+import useOnClickOutside from './hooks';
+import './App.css';
 import GlobalStyles from './global';
 import theme from './theme';
 import { Burger, Menu } from './components';
 
 function App() {
   const [burgerOpen, setBurgerOpen] = useState(false);
+
+  const node = useRef();
+  useOnClickOutside(node, () => setBurgerOpen(false));
   return (
     <ThemeProvider theme={theme}>
       <>
@@ -15,7 +19,7 @@ function App() {
           <h1>test</h1>
           <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
         </div>
-        <div>
+        <div ref={node}>
           <Burger burgerOpen={burgerOpen} setBurgerOpen={setBurgerOpen} />
           <Menu burgerOpen={burgerOpen} setBurgerOpen={setBurgerOpen} />
         </div>

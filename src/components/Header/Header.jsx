76d65b656de8b1dda5ab21d3136ad989d80760c0
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-30 18:33:14 -0300

    add avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import StyledHeader from './Header.styled';
+import avatar from '../../images/avatar.jpg';
 
 function Header() {
   return (
     <StyledHeader>
-      <h1>Olá, Sou Leonardo Ferreira, estudante de desenvolvimento Web</h1>
+      <img src={avatar} alt="leonardo" />
+      <h1>Olá, Sou Arnold Schaznegger, estudante de desenvolvimento Web</h1>
       <h2>
         Apaixonado por tecnologia e inovação e isso foi o que me levou a
         estudar Desenvolvimentode Software.
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -6,14 +6,25 @@ const StyledHeader = styled.header`
   min-width: 100vw;
   display: flex;
   flex-direction: column;
+  align-items: center;
   padding: 5em 2em;
 
   h1 {
     text-align: center;
+    font-size: 1.6em;
+    margin: 1em 0;
   }
 
   h2 {
-    text-align: justify;
+    text-align: center;
+    font-weight: 400;
+    font-size: 1.2em;
+  }
+
+  img {
+    display: block;
+    max-width: 90px;
+    border-radius: 50%;
   }
 `;
 

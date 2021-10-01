47010974f8d3e25aee97237125a3ad27e2dbb176
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-01 13:31:54 -0300

    linkedin github links

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -4,7 +4,32 @@ const StyledFooter = styled.footer`
   background-color: ${({ theme }) => theme.primaryLight};
   color: ${({ theme }) => theme.primaryDark};
   box-shadow: 9px 0 13px ${({ theme }) => theme.primaryDarkShadow};
-  height: 15vh;
+  min-height: 15vh;
+  padding: 2em 1em;
+
+  h3 {
+    text-align: center;
+    font-size: 2em;
+    margin-bottom: 0.3em;
+  }
+
+  a:hover {
+    opacity: 80%;
+    transition: 100ms;
+  }
+
+  div {
+    width: 160px;
+    height: 6em;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: flex-start;
+  }
+
+  img {
+    width: 42px;
+  }
 `;
 
 export default StyledFooter;

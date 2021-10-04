df7e79859dd2267af6ac909209c803db57336bc9
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-04 16:01:33 -0300

    close menu on click

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { bool } from 'prop-types';
+import { bool, func } from 'prop-types';
 import StyledMenu from './Menu.styled';
 
-function Menu({ burgerOpen }) {
+function Menu({ burgerOpen, setBurgerOpen }) {
   return (
     <StyledMenu burgerOpen={burgerOpen}>
-      <a href="/#projects">
+      <a href="/#projects" onClick={() => setBurgerOpen(false)}>
         <span role="img" aria-label="projects">&#x1f4f1;</span>
         Projetos
       </a>
-      <a href="/#contact">
+      <a href="/#contact" onClick={() => setBurgerOpen(false)}>
         <span role="img" aria-label="contact">&#x1f4e9;</span>
         Contate-me
       </a>
@@ -21,4 +21,5 @@ export default Menu;
 
 Menu.propTypes = {
   burgerOpen: bool.isRequired,
+  setBurgerOpen: func.isRequired,
 };
